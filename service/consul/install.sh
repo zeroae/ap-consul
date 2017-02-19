57ecd47404c52974435882cc460294c9cc5208bf
Patrick Sodré <sodre@sodre.co>
2017-02-19 02:16:11 -0500

    Upgraded code to use ap-light v0.1.0 features.

--- service/consul/install.sh
+++ service/consul/install.sh
@@ -3,9 +3,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mv $DIR/assets/consul.json /etc/consul.d/00_default.json
-mv $DIR/assets/containerpilot.json /etc/containerpilot.d/99_service.json
-
 ln -sf $DIR/assets/consul-manage /sbin
 
 exit 0
